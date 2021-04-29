37f10bd2f12f37036ee567becd868391dc09b3b2
antibantique <svecha@protonmail.com>
2021-04-29 18:07:47 +0300

    Add EntryProxy to parser

--- app/parser.py
+++ app/parser.py
@@ -1,9 +1,10 @@
 import logging
+import time
 
 import httpx
 import feedparser as fp
 
-from typing import List
+from typing import List, Optional
 
 from feedparser import FeedParserDict
 
@@ -11,6 +12,43 @@ from feeds import Feed
 
 # Add exceptions in feed parsing sinitize filter
 fp.sanitizer._HTMLSanitizer.acceptable_elements -= {'img', 'em'}
+
+
+class EntryProxy:
+
+    __slots__ = (
+        'entry',
+        'title',
+        'url',
+        'summary',
+        'published_parsed',
+        'update_parsed',
+        'published_date',
+    )
+
+    def __init__(self, entry: FeedParserDict):
+        self.entry = entry
+        self.title = entry.title
+        self.url = entry.link
+        self.summary = entry.summary
+        self.published_parsed = entry.get('published_parsed')
+        self.update_parsed = entry.get('update_parsed')
+        self.published_date = self._define_published_date(
+            self.published_parsed,
+            self.update_parsed
+        )
+
+    @staticmethod
+    def _define_published_date(
+            published: time.struct_time,
+            updated: time.struct_time
+            ) -> Optional[float]:
+        if published or updated:
+            published_date = time.mktime(published or updated)
+        else:
+            published_date = None
+
+        return published_date
 
 
 async def get_feed(feed: Feed) -> bytes:
@@ -35,9 +73,12 @@ async def get_feed(feed: Feed) -> bytes:
     return feed_data
 
 
-async def parse(feed: Feed) -> List[FeedParserDict]:
+async def parse(feed: Feed) -> List[EntryProxy]:
     feed_data = await get_feed(feed)
 
-    parsed_entries = [] if feed_data is None else fp.parse(feed_data).entries
+    if feed_data is None:
+        parsed_entries = []
+    else:
+        parsed_entries = [EntryProxy(e) for e in fp.parse(feed_data).entries]
 
     return parsed_entries
