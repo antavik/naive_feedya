fb133a8e11c3dfdd0158eb878dccfdffad43c227
antibantique <svecha@protonmail.com>
2021-05-11 18:36:09 +0300

    Update app accordint to oauth login

--- app/web/app.py
+++ app/web/app.py
@@ -1,12 +1,30 @@
 import settings
+import user
 
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
+from typing import Optional, Union
+
 from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestFormStrict,
+)
+from starlette.responses import RedirectResponse
 
-from manager import get_base_page, get_tab_sub_page, update_feed_classifier
+from manager import (
+    get_base_page,
+    get_tab_sub_page,
+    update_feed_classifier,
+    get_login_sub_page,
+)
+from .exceptions import InvalidCredentialsException, EntryURLNotFoundException
 
-app = FastAPI(title='Naive Feedya')
+APP = FastAPI(title=settings.API_NAME)
+APP.mount('/feed/static', StaticFiles(directory=str(settings.STATIC_PATH)), name='static')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/oauth/token', auto_error=False)
 
 
 class UserFeedback(BaseModel):
@@ -16,10 +34,35 @@ class UserFeedback(BaseModel):
     entry_language: str
 
 
-@app.get(
+@APP.get(
+    '/oauth/login',
+    response_class=HTMLResponse,
+    summary='Get login sub-page'
+)
+async def get_login_page():
+    response = await get_login_sub_page()
+
+    return response
+
+
+@APP.put(
+    '/oauth/token',
+    summary='Put credentials'
+)
+async def login_user(form_data: OAuth2PasswordRequestFormStrict = Depends()):
+    if not user.is_valid_credentials(form_data.username, form_data.password):
+        raise InvalidCredentialsException
+    
+    token, _ = user.generate_token()
+    response = {'access_token': str(token), 'token_type': 'bearer'}
+
+    return response
+
+
+@APP.get(
     '/feed/news/',
     response_class=HTMLResponse,
-    summary='Get rendered news html page',
+    summary='Get rendered news html page'
 )
 async def get_news_page():
     response = await get_base_page(settings.NEWS)
@@ -27,21 +70,35 @@ async def get_news_page():
     return response
 
 
-@app.get(
+@APP.get(
     '/feed/news/tab/',
-    response_class=HTMLResponse,
-    summary='News sub-page for main feed page',
+    response_class=Union[HTMLResponse, RedirectResponse],
+    summary='Get news sub-page for main feed page or redirect login sub-page'
 )
-async def get_news_tab_sub_page(last_hours: int):
-    response = await get_tab_sub_page(settings.NEWS, last_hours)
+async def get_news_tab_sub_page(
+        last_hours: int, 
+        token: Optional[str] = Depends(oauth2_scheme)
+        ):
+    if token is not None and user.is_valid_token(token):
+        content = await get_tab_sub_page(settings.NEWS, last_hours)
+        response = HTMLResponse(
+            content=content,
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+    else:
+        get_login_page_url = APP.url_path_for('get_login_page')
+        response = RedirectResponse(
+            url=get_login_page_url,
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
 
     return response
 
 
-@app.get(
+@APP.get(
     '/feed/spam/',
     response_class=HTMLResponse,
-    summary='Get rendered spam html page',
+    summary='Get rendered spam html page'
 )
 async def get_spam_page():
     response = await get_base_page(settings.SPAM)
@@ -49,24 +106,53 @@ async def get_spam_page():
     return response
 
 
-@app.get(
+@APP.get(
     '/feed/spam/tab/',
     response_class=HTMLResponse,
-    summary='Spam sub-page for main feed page',
+    summary='Get spam sub-page for main feed page or redirect login sub-page'
 )
-async def get_spam_tab_sub_page(last_hours: int):
-    response = await get_tab_sub_page(settings.SPAM, last_hours)
+async def get_spam_tab_sub_page(
+        last_hours: int, 
+        token: Optional[str] = Depends(oauth2_scheme)
+        ):
+    if token is not None and user.is_valid_token(token):
+        content = await get_tab_sub_page(settings.SPAM, last_hours)
+        response = HTMLResponse(
+            content=content,
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+    else:
+        get_login_page_url = APP.url_path_for('get_login_page')
+        response = RedirectResponse(
+            url=get_login_page_url,
+            headers={
+                'WWW-Authenticate': 'Bearer',
+            }
+        )
 
     return response
 
 
-@app.put(
+@APP.put(
     '/feed/update',
     response_class=HTMLResponse,
-    summary='Update feed classificator and entry label',
+    summary='Update feed classificator and entry label'
 )
-async def update(feedback: UserFeedback):
-    if await update_feed_classifier(feedback) is None:
-        raise HTTPException(status_code=404, detail='Feed entry URL not found')
+async def update(
+        feedback: UserFeedback,
+        token: Optional[str] = Depends(oauth2_scheme)
+        ):
+    if token is not None and user.is_valid_token(token):
+        if await update_feed_classifier(feedback) is None:
+            raise EntryURLNotFoundException
 
-    return '✅' if feedback.entry_is_valid else '🚫'
+        content = '✅' if feedback.entry_is_valid else '🚫'
+        response = f'<span>{content}</span>'
+    else:
+        get_login_page_url = APP.url_path_for('get_login_page')
+        response = RedirectResponse(
+            url=get_login_page_url,
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+
+    return response
