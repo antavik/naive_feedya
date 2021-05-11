eaf43f3fc050908eb5561227c25e369671d4e195
antibantique <svecha@protonmail.com>
2021-05-11 17:33:50 +0300

    Add render_login_sub_page in rendering

--- app/web/rendering.py
+++ app/web/rendering.py
@@ -19,10 +19,7 @@ ENVIRONMENT.filters['format_datetime'] = utils.format_datetime
 ENVIRONMENT.filters['reverse_empty_feeds'] = utils.reverse_empty_feeds
 
 
-async def render_base_page(
-        entry_type: str,
-        weather: str
-        ) -> str:
+async def render_base_page(entry_type: str) -> str:
     tamplate = ENVIRONMENT.get_template(settings.BASE_TEMPLATE_FILENAME)
 
     title_bg_color, title_font_color = utils.color_pairs_randomizer()
@@ -31,8 +28,7 @@ async def render_base_page(
         title_bg_color=title_bg_color,
         title_font_color=title_font_color,
         feed_datetime=datetime.datetime.now(),
-        entry_type=entry_type,
-        weather=weather
+        entry_type=entry_type
     )
 
     return html_page
@@ -50,5 +46,13 @@ async def render_tab_sub_page(
         last_hours=last_hours,
         feeds=feeds
     )
+
+    return html_page
+
+
+async def render_login_sub_page() -> str:
+    template = ENVIRONMENT.get_template(settings.LOGIN_TEMPLATE_FILENAME)
+
+    html_page = await template.render_async()
 
     return html_page
