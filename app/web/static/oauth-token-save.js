aa9d7bbf2cacdd619ebd77cfb5e15f4c7b5bcf36
antibantique <svecha@protonmail.com>
2021-05-16 13:28:46 +0300

    Small refactoring in oauth-token-save ext

--- app/web/static/oauth-token-save.js
+++ app/web/static/oauth-token-save.js
@@ -4,6 +4,7 @@ htmx.defineExtension('oauth-token-save', {
         if ('access_token' in tokenMeta && 'token_type' in tokenMeta) {
             window.sessionStorage.setItem('access_token', tokenMeta['access_token']);
             window.sessionStorage.setItem('token_type', tokenMeta['token_type']);
+
             location.reload();
         }
         return '<center><p>Reloading...</p></center>';
