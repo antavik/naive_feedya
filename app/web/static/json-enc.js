581531266b89ebea7ac666b96228eb07fecabfb0
antibantique <svecha@protonmail.com>
2022-06-04 12:45:22 +0800

    fix style in json-enc js

--- app/web/static/json-enc.js
+++ app/web/static/json-enc.js
@@ -1,11 +1,11 @@
 htmx.defineExtension('json-enc', {
     onEvent: function (name, evt) {
-        if (name === "htmx:configRequest") {
-            evt.detail.headers['Content-Type'] = "application/json";
+        if (name === 'htmx:configRequest') {
+            evt.detail.headers['Content-Type'] = 'application/json';
         }
     },
-    
-    encodeParameters : function(xhr, parameters, elt) {
+
+    encodeParameters: function(xhr, parameters, elt) {
         xhr.overrideMimeType('text/json');
         return (JSON.stringify(parameters));
     }
